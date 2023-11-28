1bb89177677dae7051cda56431d99964a97eb30e
Mikaela-D <G00401392@atu.ie>
2023-11-28 18:38:20 +0000

    Created new project

---
+++ src/main/java/ie/atu/lab6_docker/Lab6DockerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab6_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6DockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab6_docker/Lab6DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab6_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
